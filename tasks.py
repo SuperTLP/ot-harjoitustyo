acfe2d35e8503a4091dad2e6e117703577b2a4a8
Riku Mattila <riku.p.mattila@gmail.com>
2022-11-22 20:28:32 +0200

    removed poetry run from tasks

--- tasks.py
+++ tasks.py
@@ -2,11 +2,11 @@ from invoke import task
 
 @task
 def start(c):
-    c.run("poetry run python3 src/index.py")
+    c.run("python3 src/index.py")
 @task
 def test(c):
-    c.run("poetry run pytest src")
+    c.run("pytest src")
 @task
 def coverage_report(c):
-    c.run("poetry run coverage run --branch -m pytest src")
-    c.run("poetry run coverage html")
+    c.run("coverage run --branch -m pytest src")
+    c.run("coverage html")
