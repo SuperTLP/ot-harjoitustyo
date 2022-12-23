854e3e2bf8245955479b89b15a55acda313ae9a7
Riku Mattila <riku.p.mattila@gmail.com>
2022-12-23 20:30:26 +0200

    added new test and new coverage report

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -7,23 +7,42 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.db=MagicMock()
         self.db.new=MagicMock()
-    def test_right_out_of_bound(self):
         snake=MagicMock()
         snake.advance=MagicMock()
-        snake.advance.return_value=[[1, 12], [1, 13], [1, 14]]
-        game = Game(snake, self.db)
+        self.snake=snake
+
+    def test_right_out_of_bound(self):
+        self.snake.advance.return_value=[[1, 12], [1, 13], [1, 14]]
+        game = Game(self.snake, self.db)
         game.start("Riku", "easy")
         game.advance()
         self.db.new.assert_called_with("Riku", 0, "easy")
         self.assertEqual(game.game_over, True)
+
     def test_normal_advance(self):
-        snake=MagicMock()
-        snake.advance=MagicMock()
-        snake.advance.return_value=[[1, 5], [1, 6], [1, 7]]
-        game = Game(snake, self.db)
+        self.snake.advance.return_value=[[1, 5], [1, 6], [1, 7]]
+        game = Game(self.snake, self.db)
         game.start("Riku", "easy")
         game.advance()
         expected_snake_coordinates=[[1, 5], [1, 6], [1, 7]]
         for element in expected_snake_coordinates:
             self.assertEqual(game.game_matrix[element[0]][element[1]].type, "snake")
+
+    def test_consume_gives_points(self):
+        self.snake.advance.return_value=[[1, 4], [1, 5], [1, 6]]
+        game=Game(self.snake,self.db)
+        game.start("Riku","hard")
+        treat=MagicMock()
+        treat.consume=MagicMock()
+        treat.points=15
+        treat.type="treat"
+        game.game_matrix[1][6]=treat
+        game.advance()
+        self.assertEqual(game.points,15)
+
+    
+    
+
+
+
 
